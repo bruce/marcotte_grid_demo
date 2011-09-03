e490719654164166557663db3cc2059b1f915121
Bruce Williams <bruce@codefluency.com>
2011-09-03 16:42:52 -0700

    Basic demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarcotteTest::Application.config.secret_token = '1a1232cf0653d5b503be2a8caddf334de7d85dab9ad1ad25a3f92d17be97463b183275d8498761582fcba56d6b39c278162fe4a9c968caf0c502672c5acf9628'
